41696e9f3d99efbc3cfe84b5d0b7c495e4255067
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-11-12 13:29:56 +1000

    - massacred line endings in the solution
    - added implementation of change password. I'm not very happy with it - it does not do client side validation, which for some reason would not work, it does not notify user of success and probably has an entire list of flaws

--- src/Suteki.TardisBank/Controllers/AdminController.cs
+++ src/Suteki.TardisBank/Controllers/AdminController.cs
@@ -1,17 +1,22 @@
 using System.Web.Mvc;
 using Suteki.TardisBank.Helpers;
 using Suteki.TardisBank.Model;
+using Suteki.TardisBank.Mvc;
 using Suteki.TardisBank.Services;
+using Suteki.TardisBank.ViewModel;
 
 namespace Suteki.TardisBank.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         readonly IUserService userService;
+        readonly IFormsAuthenticationService formsAuthenticationService;
 
-        public AdminController(IUserService userService)
+        public AdminController(IUserService userService, IFormsAuthenticationService formsAuthenticationService)
         {
             this.userService = userService;
+            this.formsAuthenticationService = formsAuthenticationService;
         }
 
         [HttpGet]
@@ -42,6 +47,44 @@ namespace Suteki.TardisBank.Controllers
             userService.DeleteUser(parent.Id);
 
             return RedirectToAction("Logout", "User");
+        }
+
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [UnitOfWork]
+        public ActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            
+            var oldHashedPassword = GetHashedPassword(model.OldPassword);
+            bool passwordIsOk = false;
+            if (oldHashedPassword == userService.CurrentUser.Password)
+            {
+                passwordIsOk = true;
+            }
+            else
+            {
+                ModelState.AddModelError("OldPassword", "The password you provided is invalid");
+            }
+            if(ModelState.IsValid && passwordIsOk)
+            {
+                var newHashedPassword = GetHashedPassword(model.NewPassword);
+                userService.CurrentUser.ResetPassword(newHashedPassword);
+                // TODO: we should have also a flash message saying it's been successful
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        private string GetHashedPassword(string password)
+        {
+            return formsAuthenticationService.HashAndSalt(
+                userService.CurrentUser.UserName,
+                password);
         }
     }
 }
